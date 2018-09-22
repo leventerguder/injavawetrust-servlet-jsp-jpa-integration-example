579aabba2f61e01e3b2b2b3ca538b47c6021d082
leventerguder <erguder.levent@gmail.com>
2018-09-22 16:14:32 +0300

    code to interface :)

--- ServletAndJPA/src/service/EmployeeServiceImpl.java
+++ ServletAndJPA/src/service/EmployeeServiceImpl.java
@@ -3,34 +3,35 @@ package service;
 import java.util.List;
 
 import model.Employee;
+import dao.EmployeeDAO;
 import dao.EmployeeDAOImpl;
 
 public class EmployeeServiceImpl implements EmployeeService {
 
-	private EmployeeDAOImpl employeeDAOImpl;
+	private EmployeeDAO employeeDAO;
 
 	public EmployeeServiceImpl() {
-		employeeDAOImpl = new EmployeeDAOImpl();
+		employeeDAO = new EmployeeDAOImpl();
 	}
 
 	@Override
 	public Employee insertEmployee(Employee employee) {
-		return employeeDAOImpl.insertEmployee(employee);
+		return employeeDAO.insertEmployee(employee);
 	}
 
 	@Override
 	public Employee findEmployee(int id) {
-		return employeeDAOImpl.findEmployee(id);
+		return employeeDAO.findEmployee(id);
 	}
 
 	@Override
 	public List<Employee> findAllEmployees() {
-		return employeeDAOImpl.findAllEmployees();
+		return employeeDAO.findAllEmployees();
 
 	}
 
 	@Override
 	public void removeEmployee(int id) {
-		employeeDAOImpl.removeEmployee(id);
+		employeeDAO.removeEmployee(id);
 	}
 }
